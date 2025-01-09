5060328986413dce312873f08b13630d2161625c
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2025-01-09 11:08:19 +0800

    - Added "helpers.py" for apology function (show elegant errors - Added "apology.html" included the spiderman glasses meme - Updated "app.py" to make session be permanent (so user would have to clear their cookies) - Tested the play_now route to use the apology

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template, session, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+from helpers import apology
+
 app = Flask(__name__)
 
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///binary_speed.db'
 app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_PERMANENT'] = True
 db = SQLAlchemy(app)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/")
 def play():
@@ -18,7 +29,7 @@ def leaderboard():
 
 @app.route('/play_now')
 def play_now():
-    return "nada"
+    return apology("TODO")
 
 @app.route('/set_username', methods=['POST'])
 def set_username():
